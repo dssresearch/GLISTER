2452034cbe5e029ea6ea0c1608293240c6e08d3b
dssresearcher <>
2020-12-10 16:53:29 +0530

    fast linear

--- models/set_function_act_learn.py
+++ models/set_function_act_learn.py
@@ -349,6 +349,151 @@ class Small_GlisterAct_Linear_SetFunction(object):
         print("Naive greedy total time:", time.time() - t_ng_start)
         return list(greedySet)
 
+class Small_GlisterAct_Linear_SetFunction_Closed_Vect(object):
+    def __init__(self, x_val, y_val, model, loss_criterion,
+                 loss_nored, eta, device, num_classes):
+        self.x_val = x_val.to(device)
+        self.y_val = y_val.to(device)
+        self.model = model
+        self.loss = loss_criterion  # Make sure it has reduction='none' instead of default
+        self.loss_nored = loss_nored
+        self.eta = eta  # step size for the one step gradient update
+        # self.opt = optimizer
+        self.device = device
+        self.grads_per_elem = None
+        self.theta_init = None
+        self.num_classes = num_classes
+        self.numSelected = 0
+
+    def _compute_per_element_grads(self, x_trn, y_trn, theta_init):
+        self.model.load_state_dict(theta_init)
+        embDim = self.model.get_embedding_dim()
+        with torch.no_grad():
+            out, l1 = self.model(x_trn, last=True)
+            data = F.softmax(out, dim=1)
+        l1_grads = torch.zeros(x_trn.shape[0], embDim * self.num_classes).to(self.device)
+        tmp_tensor = torch.zeros(x_trn.shape[0], self.num_classes).to(self.device)
+        tmp_tensor.scatter_(1, y_trn.view(-1, 1), 1)
+        outputs = tmp_tensor
+        l0_grads = data - outputs
+        # l1_grads = torch.zeros(self.batch_size, self.num_classes, l1.shape[1]).to(self.device)
+        '''for i in range(x_trn.shape[0]):
+            for j in range(self.num_classes):
+                l1_grads[i][(j * embDim): ((j + 1) * embDim)] = l0_grads[i, j] * l1[i]'''
+
+        l0_expand = torch.repeat_interleave(l0_grads, embDim, dim=1) 
+        l1_grads = l0_expand * l1.repeat(1,self.num_classes)
+                
+        '''ind = torch.arange(0,embDim,device=self.device).repeat(x_trn.shape[0],1)
+        max_ind = ind + (y_trn*embDim)[:,None]
+
+        soft_max = -1*torch.repeat_interleave(data, l1.shape[1], dim=1) 
+        l1_grads = (l1.repeat(1,self.num_classes) * soft_max).scatter_add_(1, max_ind, l1)'''
+
+        torch.cuda.empty_cache()
+        #print("Per Element Gradient Computation is Completed")
+        self.grads_per_elem = torch.cat((l0_grads, l1_grads), dim=1)
+
+    def _update_grads_val(self, theta_init, grads_currX=None, first_init=False):
+        self.model.load_state_dict(theta_init)
+        self.model.zero_grad()
+        if first_init:
+            with torch.no_grad():
+                self.init_out, self.init_l1 = self.model(self.x_val, last=True)
+                scores = F.softmax(self.init_out, dim=1)
+                one_hot_label = torch.zeros(len(self.y_val), self.num_classes).to(self.device)
+                one_hot_label.scatter_(1, self.y_val.view(-1, 1), 1)
+                l0_grads = scores - one_hot_label
+                embDim = self.model.get_embedding_dim()
+                l1_grads = torch.zeros(self.init_l1.shape[0], self.num_classes * embDim).to(self.device)
+                #l1_grads = torch.zeros(l1.shape[0], self.num_classes, l1.shape[1]).to(self.device)
+                '''for i in range(self.init_l1.shape[0]):
+                    for j in range(self.num_classes):
+                        l1_grads[i, (j * embDim):((j+1) * embDim)] = l0_grads[i, j] * self.init_l1[i]'''
+
+                l0_expand = torch.repeat_interleave(l0_grads, embDim, dim=1) 
+                l1_grads = l0_expand * self.init_l1.repeat(1,self.num_classes)
+                
+        # populate the gradients in model params based on loss.
+        
+        elif grads_currX is not None:
+            # update params:
+            with torch.no_grad():
+                embDim = self.model.get_embedding_dim()
+                out = torch.zeros(self.init_out.shape[0], self.num_classes).to(self.device)
+                for j in range(self.num_classes):
+                    out[:, j] = self.init_out[:, j] -(1 * self.eta * (torch.matmul(self.init_l1, grads_currX[0][(j*embDim)+self.num_classes:((j+1)*embDim)+self.num_classes].view(-1, 1)) + grads_currX[0][j])).view(-1)
+                
+                scores = F.softmax(out, dim=1)
+                one_hot_label = torch.zeros(len(self.y_val), self.num_classes).to(self.device)
+                one_hot_label.scatter_(1, self.y_val.view(-1, 1), 1)
+                l0_grads = scores - one_hot_label
+                l1_grads = torch.zeros(self.init_l1.shape[0], self.num_classes * embDim).to(self.device)
+                # l1_grads = torch.zeros(l1.shape[0], self.num_classes, l1.shape[1]).to(self.device)
+                '''for i in range(self.init_l1.shape[0]):
+                    for j in range(self.num_classes):
+                        l1_grads[i][(j * embDim):((j + 1) * embDim)] = l0_grads[i, j] * self.init_l1[i]'''
+                l0_expand = torch.repeat_interleave(l0_grads, embDim, dim=1) 
+                l1_grads = l0_expand * self.init_l1.repeat(1,self.num_classes)
+
+        self.grads_val_curr = torch.cat((l0_grads, l1_grads), dim=1).mean(dim=0).view(-1, 1)
+
+    def eval_taylor_modular(self, grads):
+        grads_val = self.grads_val_curr
+        with torch.no_grad():
+            gains = torch.matmul(grads, grads_val)
+        return gains
+
+    # Updates gradients of set X + element (basically adding element to X)
+    # Note that it modifies the inpute vector! Also grads_X is a list! grad_e is a tuple!
+    def _update_gradients_subset(self, grads_X, element):
+        grads_e = self.grads_per_elem[element]
+        grads_X += grads_e
+
+    # Same as before i.e full batch case! No use of dataloaders here!
+    # Everything is abstracted away in eval call
+    def naive_greedy_max(self, x_trn, y_trn, budget, theta_init):
+        start_time = time.time()
+        x_trn = x_trn.to(self.device)
+        y_trn = y_trn.to(self.device)
+        self._compute_per_element_grads(x_trn, y_trn, theta_init)
+        end_time = time.time()
+        #print("Per Element gradient computation time is: ", end_time - start_time)
+        start_time = time.time()
+        self._update_grads_val(theta_init, first_init=True)
+        end_time = time.time()
+        #print("Updated validation set gradient computation time is: ", end_time - start_time)
+        # Dont need the trainloader here!! Same as full batch version!
+        self.numSelected = 0
+        grads_currX = []  # basically stores grads_X for the current greedy set X
+        greedySet = list()
+        remainSet = list(range(x_trn.shape[0]))
+        t_ng_start = time.time()  # naive greedy start time
+        subset_size = int((len(self.grads_per_elem) / budget) * math.log(100))
+        while (self.numSelected < budget):
+            # Try Using a List comprehension here!
+            t_one_elem = time.time()
+            subset_selected = list(np.random.choice(np.array(list(remainSet)), size=subset_size, replace=False))
+            rem_grads = self.grads_per_elem[subset_selected]
+            gains = self.eval_taylor_modular(rem_grads)
+            # Update the greedy set and remaining set
+            bestId = subset_selected[torch.argmax(gains).item()]
+            greedySet.append(bestId)
+            remainSet.remove(bestId)
+            self.numSelected += 1
+            # Update info in grads_currX using element=bestId
+            if self.numSelected > 1:
+                self._update_gradients_subset(grads_currX, bestId)
+            else:  # If 1st selection, then just set it to bestId grads
+                grads_currX = self.grads_per_elem[bestId].view(1, -1)  # Making it a list so that is mutable!
+            # Update the grads_val_current using current greedySet grads
+            self._update_grads_val(theta_init, grads_currX)
+            #if (self.numSelected - 1)%1000 == 0:
+                # Printing bestGain and Selection time for 1 element.
+                #print("numSelected:", self.numSelected, "Time for 1:", time.time() - t_one_elem)
+        #print("Naive greedy total time:", time.time() - t_ng_start)
+        return list(greedySet)
+
 
 class Small_GlisterAct_Linear_SetFunction_Closed(object):
     def __init__(self, x_val, y_val, model, loss_criterion,
